055aa5a2b1d24149e4a0602de877e717ade1a969
Jennifer Kron <jenniferkron@gmail.com>
2016-08-07 11:50:45 -0400

    added test to check that a number on the hour isn't rounded up and that the time is a number

--- spec/babysittertests.js
+++ spec/babysittertests.js
@@ -5,10 +5,17 @@ describe('jasmine test', function(){
 
 });
 
-
 describe('time should be rounded up', function(){
-  it('should return 4 when time is 3:01', function() {
+  it('should return 8 when time is 7:01', function() {
     expect(formatTime('7:01')).toBe(8);
   });
+  it('should return 7 when time is 7:00)', function() {
+    expect(formatTime('7:00')).toBe(7);
+  });
+});
 
+describe('time should be a number', function(){
+  it('the type of time should equal a number', function() {
+    expect(typeof(formatTime('9:40'))).toEqual('number');
+  });
 });
