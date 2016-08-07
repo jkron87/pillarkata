86a493da529fe429bd240652221d41113c50847b
Jennifer Kron <jenniferkron@gmail.com>
2016-08-07 12:09:04 -0400

    added before bedtime function and test

--- spec/babysittertests.js
+++ spec/babysittertests.js
@@ -19,3 +19,21 @@ describe('time should be a number', function(){
     expect(typeof(formatTime('9:40'))).toEqual('number');
   });
 });
+
+describe('twelve should be added to the time if less than 5', function(){
+  it('should return 13 if time is 1:00', function() {
+    expect(formatTime('1:00')).toBe(13);
+  });
+  it('should return 14 if time is 1:01', function() {
+    expect(formatTime('1:01')).toBe(14);
+  });
+});
+
+describe('babysitter earns $12/hour before bedtime', function(){
+  it('should return $24 if start time is 5pm and bedtime is 7pm', function() {
+    expect(calculateBeforeBed(5, 7)).toBe(24);
+  });
+  it('should return $36 if start time is 9pm and bedtime is 1am', function() {
+    expect(calculateBeforeBed(9, 13)).toBe(36);
+  });
+});
--- src/babysitterkata.js
+++ src/babysitterkata.js
@@ -1,3 +1,28 @@
+var scheduleRange = {
+  start: 5,
+  end: 16,
+}
+
+var payrate = {
+  beforeBed: 12,
+  sleeping: 8,
+  afterMidnight: 16
+}
+
+var calculateBeforeBed = function (start, bedtime) {
+    if (bedtime > start && bedtime < 12){
+    var beforeBedEarned = (bedtime - start) * payrate.beforeBed;
+    return beforeBedEarned;
+  } else if (bedtime > 12) {
+    var beforeBedEarned = (12 - start) * payrate.beforeBed;
+    return beforeBedEarned;
+  } else {
+    return 0;
+  }
+}
+
+
+
 function formatTime(time){
   if (time.indexOf('00') === -1){
     time = Number(time.split(":").shift()) + 1;
